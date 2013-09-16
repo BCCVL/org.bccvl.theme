92ddcfbc73786cb85dd4ee8a24104004bd831aea
Gerhard Weis <gerhard.weis@gmail.com>
2013-09-16 14:15:40 +1000

    add simple robot test example (which currently fails, as it looks for default unthemed plone content)

---
+++ org/bccvl/theme/tests/test_robot.py
@@ -0,0 +1,14 @@
+import unittest2 as unittest
+
+import robotsuite
+from org.bccvl.theme.testing import BCCVL_THEME_FUNCTIONAL_TESTING
+from plone.testing import layered
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTests([
+        layered(robotsuite.RobotTestSuite('test_first.robot'),
+                layer=BCCVL_THEME_FUNCTIONAL_TESTING),
+    ])
+    return suite
