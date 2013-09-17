d09909e304aadc235b822c24e615ca0ed6f1d010
Gerhard Weis <gerhard.weis@gmail.com>
2013-09-17 20:08:25 +1000

    make theme functional tests work with full async setup

--- org/bccvl/theme/testing.py
+++ org/bccvl/theme/testing.py
@@ -4,8 +4,11 @@ from plone.app.robotframework.testing import (
     LIVESEARCH_FIXTURE,
     AUTOLOGIN_LIBRARY_FIXTURE,
     REMOTE_LIBRARY_BUNDLE_FIXTURE
-    )
-from org.bccvl.site.testing import BCCVL_FIXTURE
+)
+from org.bccvl.site.testing import (
+    BCCVL_FIXTURE, BCCVL_ASYNC_FIXTURE,
+    BCCVLAsyncFunctionalTesting
+)
 from plone.app.testing import PloneSandboxLayer
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import FunctionalTesting
@@ -26,38 +29,18 @@ BCCVL_THEME_INTEGRATION_TESTING = IntegrationTesting(
     name="BCCVLThemeFixture:Integration")
 
 
-# TODO: async functional testing not yet ready to use:
-TEST_ASYNC = False
-if not TEST_ASYNC:
+BCCVL_THEME_FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(BCCVL_THEME_FIXTURE,
+           AUTOLOGIN_LIBRARY_FIXTURE,
+           z2.ZSERVER_FIXTURE),
+    name="BCCVLThemeFixture:Functional")
 
-    BCCVL_THEME_FUNCTIONAL_TESTING = FunctionalTesting(
-        bases=(BCCVL_THEME_FIXTURE,
-               AUTOLOGIN_LIBRARY_FIXTURE,
-               z2.ZSERVER_FIXTURE),
-        name="BCCVLThemeFixture:Functional")
 
-else:
+BCCVL_THEME_ASYNC_FIXTURE = BCCVLThemeLayer(bases=(BCCVL_ASYNC_FIXTURE, ),
+                                            name='BCCVLThemeAsyncLayer')
 
-    from plone.app.async.testing import AsyncLayer
-    from plone.app.async.testing import AsyncFunctionalTesting
-    from plone.app.async.testing import registerAsyncLayers
-
-    class BCCVLThemeAsyncLayer(AsyncLayer):
-
-        defaultBases = (BCCVL_THEME_FIXTURE, )
-
-        def setUpPloneSite(self, portal):
-            # do nothing here, all the setup already done in
-            # base layers
-            pass
-
-    BCCVL_THEME_ASYNC_FIXTURE = BCCVLThemeAsyncLayer()
-
-    BCCVL_THEME_FUNCTIONAL_TESTING = AsyncFunctionalTesting(
-        bases=(BCCVL_THEME_ASYNC_FIXTURE,
-               AUTOLOGIN_LIBRARY_FIXTURE,
-               z2.ZSERVER_FIXTURE),
-        name="BCCVLThemeFixture:Robot")
-
-    registerAsyncLayers([BCCVL_THEME_ASYNC_FIXTURE,
-                        BCCVL_THEME_FUNCTIONAL_TESTING])
+BCCVL_THEME_ASYNC_FUNCTIONAL_TESTING = BCCVLAsyncFunctionalTesting(
+    bases=(BCCVL_THEME_ASYNC_FIXTURE,
+           AUTOLOGIN_LIBRARY_FIXTURE,
+           z2.ZSERVER_FIXTURE),
+    name="BCCVLThemeAsyncFixture:Functional")
