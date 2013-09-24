7ecf86438bd4d9ce3167c0cdd51d4f674ceb0229
Daniel Baird <daniel@danielbaird.com>
2013-09-24 20:01:48 +1000

    prelim work to viz occurrences

--- org/bccvl/theme/theme/js/bccvl-visualiser.js
+++ org/bccvl/theme/theme/js/bccvl-visualiser.js
@@ -17,6 +17,20 @@ window.bccvl.visualiser = {
         // find the visualiser debug button
         var $visualiserDebugButton = $('#visualiser_debug');
 
+        var $vizOccurs = $('.bccvl-occurrence-viz');
+        console.log($vizOccurs);
+
+        $.each($vizOccurs, function(vIndex, occur) {
+            // each occur should have a data-viz-id.. bail if it doesn't
+            var $occur = $(occur);
+            var id = $occur.attr('data-viz-id');
+            if (!id) return;
+
+            // attach some click behaviour to the thing
+            $occur.click(function() {
+                console.log(id);
+            });
+        });
     },
 
     visualise: function(dataId) {
