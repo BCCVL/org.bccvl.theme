92df78f9b4175492b7ebfadfcf028e40e761151b
Daniel Baird <daniel@danielbaird.com>
2013-09-24 17:56:37 +1000

    Adds initial 'projection experiment' UI template, with dimension count JS library

---
+++ org/bccvl/theme/theme/js/bccvl-dimension-equation.js
@@ -0,0 +1,46 @@
+
+// JS code to make BCCVL dimension equations work.
+//
+// A dimension chooser is a tag with the class .bccvl-dimension-selection
+
+window.makeBCCVLDimensionEquations = function() {
+
+    // find all the dimension choosers..
+    var selectors = $('.bccvl-dimension-selection');
+
+    // loop through each dimension chooser, setting up any dimension values in it.
+    $.each(selectors, function(dsIndex, selector) {
+        // get some jQuery objects together..
+        var $selector = $(selector);
+        var $values = $selector.find('.bccvl-dimension-value');
+
+        if ($values.length == 0) return; // bail if there's no values to update
+
+        // find the product values and remove them from the set
+        var $products = $selector.find('.bccvl-dimension-value[data-dimension-name="projProduct"]');
+        $values = $values.not($products);
+
+        // set up the relevant form fields for each dimension value
+        $.each($values, function(valIndex, value) {
+            var $value = $(value);
+            // pick out the dimension's name
+            var dName = $value.attr('data-dimension-name');
+            // bail if there's no dimension name, or it's a product.
+            if (!dName || dName == 'projProduct') return;
+
+            // find the checkboxes that correspond with that name
+            var $checkboxes = $selector.find('input[type="checkbox"][name="' + dName + '"]');
+
+            $checkboxes.change(function(evt) {
+                $value.text($checkboxes.filter(':checked').length);
+                var product = 1;
+                $.each($values, function(valueIndex, val) {
+                    product = product * parseInt($(val).text(), 10);
+                });
+                $products.text(product);
+            });
+        });
+    });
+}
+
+
