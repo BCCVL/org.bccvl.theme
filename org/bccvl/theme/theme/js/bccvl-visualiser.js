14123bb04ec8b6a064ae10502fcf16c0d990548f
Daniel Baird <daniel@danielbaird.com>
2013-09-25 14:01:44 +1000

    adds viz for occurrences, probably

--- org/bccvl/theme/theme/js/bccvl-visualiser.js
+++ org/bccvl/theme/theme/js/bccvl-visualiser.js
@@ -31,16 +31,28 @@ window.bccvl.visualiser = {
 
                 console.log("here's where I'd look up " + id + " in the OCCURRENCES_MAP that Matthew has given us.");
 
+                bccvl.visualiser.visualise(window.bccvl.lookups.occurrenceMap[id]);
+
                 evt.preventDefault();
                 return false;
             });
         });
     },
 
-    visualise: function(dataId) {
+    visualise: function(dataId, options) {
+        var opts = {    apiType: 'autodetect',
+                        apiVersion: '1',
+                        vizType: 'data_url_map'
+        };
+        // merge in the caller's options
+        if (options) {
+            for (var opt in options) { opts[opt] = options[opt]; }
+        }
+
         console.log('viz got: ', dataId);
+
         frame = $('iframe.bccvl-viz');
-        frame.attr('src', this.visualiserBaseUrl + 'api/raster/1/data_url_map?data_url=' + dataId);
+        frame.attr('src', this.visualiserBaseUrl + 'api/' + opts.apiType + '/' + opts.apiVersion + '/' + opts.vizType + '?data_url=' + dataId);
     }
 
 };
