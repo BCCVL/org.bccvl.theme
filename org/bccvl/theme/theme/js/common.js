981c56a42ffc878268b4453124311566a4a12be3
Daniel Baird <daniel@danielbaird.com>
2013-11-07 11:09:04 +1000

    working on file upload (interim push for testing, might be borken)

--- org/bccvl/theme/theme/js/common.js
+++ org/bccvl/theme/theme/js/common.js
@@ -6,18 +6,20 @@
 // except for 'js' ones, which are in a sibling
 // directory.
 
-var local = (window.location.hostname == 'localhost');
+var local = (window.location.host == 'localhost:9292');
 
 requirejs.config({
     baseUrl: (local ? 'lib' : '/++theme++org.bccvl.theme/lib'),          // load modules from the lib folder
     paths: {
-        'js':        (local ? '../js' : '/++theme++org.bccvl.theme/js'),  // bccvl stuff, which starts with js/, is in the js folder
-        'jquery':        'jquery/jquery-2.0.3',                           // Ima say 'jquery', u say jquery-x.y.z.js, dawg
-        'jquery-xmlrpc': 'jquery-xmlrpc/jquery.xmlrpc.min',
-        'bootstrap':     'bootstrap/js/bootstrap.min'
+        'js':                   (local ? '../js' : '/++theme++org.bccvl.theme/js'),  // bccvl stuff, which starts with js/, is in the js folder
+        'jquery':               'jquery/jquery-2.0.3',                               // Ima say 'jquery', u say jquery-x.y.z.js, dawg
+        'jquery-xmlrpc':        'jquery-xmlrpc/jquery.xmlrpc.min',
+        'bootstrap':            'bootstrap/js/bootstrap.min',
+        'bootstrap-fileupload': 'bootstrap-fileupload/bootstrap-fileupload.min'
     },
     shim: {
-        'bootstrap':     ['jquery'],  // bootstrap needs jquery
-        'jquery-xmlrpc': ['jquery']
+        'bootstrap':            ['jquery'],
+        'bootstrap-fileupload': ['bootstrap'],
+        'jquery-xmlrpc':        ['jquery']
     }
 });
--- org/bccvl/theme/theme/js/dataset-upload.js
+++ org/bccvl/theme/theme/js/dataset-upload.js
@@ -2,8 +2,8 @@
 //
 // main JS for the dataset upload page.
 //
-define(     ['jquery', 'bootstrap'],
-    function( $) {
+define(     ['jquery', 'bootstrap', 'bootstrap-fileupload'],
+    function( $ ) {
     // ==============================================================
         $(function() {
             // nothing to do so far.
