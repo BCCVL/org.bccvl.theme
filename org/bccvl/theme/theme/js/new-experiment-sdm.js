762e12c8df94a051b9f5224484579b2f5c17c6b3
Daniel Baird <daniel@danielbaird.com>
2013-11-14 11:41:34 +1000

    clarifications to js

--- org/bccvl/theme/theme/js/new-experiment-sdm.js
+++ org/bccvl/theme/theme/js/new-experiment-sdm.js
@@ -21,9 +21,10 @@ define(     ['jquery', 'js/bccvl-visualiser', 'js/bccvl-wizard-tabs', 'js/bccvl-
                 successClass: 'success',    // use these two Bootstrap classes for the error
                 errorClass: 'error',        // and no-error states, and it'll look pretty.
                 errors: {
-                    classHandler: function(el) { return $(el).closest('.control-group'); },
+                    // this error handling and elements make parsley errors Bookstrap friendly
+                    classHandler:  function(el) { return $(el).closest('.control-group'); },
                     errorsWrapper: '<span class=\"help-inline\"></span>',
-                    errorElem: '<span></span>'
+                    errorElem:     '<span></span>'
                 }
             });
 
