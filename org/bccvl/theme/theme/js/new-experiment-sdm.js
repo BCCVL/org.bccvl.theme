eb13f652cbb1707fba7ca25c82c6a17a6606f59d
Daniel Baird <daniel@danielbaird.com>
2013-12-09 15:21:48 +1000

    sort layers by name

--- org/bccvl/theme/theme/js/new-experiment-sdm.js
+++ org/bccvl/theme/theme/js/new-experiment-sdm.js
@@ -175,6 +175,7 @@ define(     ['jquery', 'js/bccvl-visualiser', 'js/bccvl-wizard-tabs', 'js/bccvl-
                                 // render each layer
                                 $.each(list.layers, function(layerId) {
                                     var name = layerName(layerId, list.layers[layerId]);
+                                    console.log(name);
                                     layerNames.push(name);
                                     layers[name] = renderLayerRow(token, layerId, list.layers[layerId]);
                                 });
