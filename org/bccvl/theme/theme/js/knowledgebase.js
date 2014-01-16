de5fc67733d27ba3b2c4992fe049acc703cc187b
Daniel Yazbek <daniel@intersect.org.au>
2014-01-16 17:12:18 +1100

    BCCVL-268 - Initial knowledge base themed with working breadcrumbs

---
+++ org/bccvl/theme/theme/js/knowledgebase.js
@@ -0,0 +1,14 @@
+
+//
+// main JS for the knowledgeboard page.
+//
+define(     ['jquery', 'bootstrap'],
+    function( $) {
+    // ==============================================================
+        $(function() {
+            // nothing to do so far.
+            console.log('page behaviour loaded.');
+        });
+    // ==============================================================
+    }
+);
