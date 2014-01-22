ead95b201787a0502a935f24ff42f7e8f91af537
Khanh Nguyen <khanh@intersect.org.au>
2014-01-22 17:00:38 +1100

    Clean up the reference datasets page and upload dataset page.
    Separated the Add dataset tab and reference dataset tab entirely - no longer sharing the right span
    Fix upload dataset button to correctly redirect to the upload page when on datasets sub directories
    Add javascript to determine the active tab based on the end of the URL

--- org/bccvl/theme/theme/js/dataset-list.js
+++ org/bccvl/theme/theme/js/dataset-list.js
@@ -7,10 +7,24 @@ define(     ['jquery', 'js/bccvl-search', 'js/bccvl-stretch', 'bootstrap'],
     // ==============================================================
         $(function() {
 
+        		// This sets the active tab to reference data sets if the url 
+        		// does not end with datasets
+        		var my_path = location.pathname;
+            if (!my_path.endsWith('datasets') || !my_path.endsWith('datasets/')){
+            	$('.bccvl-add-datasets').removeClass('active');
+            	$('.bccvl-my-datasets').addClass('active');
+            	$('#tab-add').removeClass('active');
+            	$('#tab-ref').addClass('active');
+            }
+
             search.init();
             stretch.init({ topPad: 60, bottomPad: 10 });
 
         });
+
+        String.prototype.endsWith = function(suffix) {
+    			return this.indexOf(suffix, this.length - suffix.length) !== -1;
+				};
     // ==============================================================
     }
 );
