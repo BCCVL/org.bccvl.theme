d211a5bc24100255f672c2044d4184b74231246b
Daniel Yazbek <daniel@intersect.org.au>
2014-02-05 17:18:02 +1100

    Theme password reset page

---
+++ org/bccvl/theme/theme/js/password-reset.js
@@ -0,0 +1,13 @@
+//
+// main JS for the password reset page.
+//
+define(     ['jquery', 'bootstrap'],
+    function( $) {
+    // ==============================================================
+        $(function() {
+            console.log('password reset page behaviour loaded.');
+
+        });
+    // ==============================================================
+    }
+);
