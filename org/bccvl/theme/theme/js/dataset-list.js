34b600896637bddb653c53bea884620803bd4edd
Khanh Nguyen <khanh@intersect.org.au>
2014-02-06 15:49:51 +1100

    Add table sorter for datasets list in datasets page

--- org/bccvl/theme/theme/js/dataset-list.js
+++ org/bccvl/theme/theme/js/dataset-list.js
@@ -2,17 +2,23 @@
 //
 // main JS for the dataset list page.
 //
-define(     ['jquery', 'js/bccvl-search', 'js/bccvl-stretch', 'js/bccvl-visualiser', 'bootstrap'],
-	function( $      ,  search          ,  stretch          ,  viz ) {
-	// ==============================================================
-		$(function() {
+define(     ['jquery', 'js/bccvl-search', 'js/bccvl-stretch', 'js/bccvl-visualiser', 'bootstrap', 'jquery-tablesorter'],
+  function( $      ,  search          ,  stretch          ,  viz ) {
+  // ==============================================================
+    $(function() {
 
-			viz.init();
-			search.init();
-			stretch.init({ topPad: 60, bottomPad: 10 });
+      viz.init();
+      search.init();
+      stretch.init({ topPad: 60, bottomPad: 10 });
 
-		});
+      $('.bccvl-datasetstable').tablesorter({
+        headers: { 
+            3: { sorter: false } // should be link column
+        }
+      });
 
-	// ==============================================================
-	}
+    });
+
+  // ==============================================================
+  }
 );
