279a7cac7baf47f2633ea92443c4ed5f9cdf890d
Daniel Yazbek <daniel@intersect.org.au>
2014-02-06 17:09:01 +1100

    Migrate datasets page to 3 separate pages with tabs.

--- org/bccvl/theme/theme/js/dataset-list.js
+++ org/bccvl/theme/theme/js/dataset-list.js
@@ -2,13 +2,12 @@
 //
 // main JS for the dataset list page.
 //
-define(     ['jquery', 'js/bccvl-search', 'js/bccvl-stretch', 'js/bccvl-visualiser', 'bootstrap', 'jquery-tablesorter'],
-  function( $      ,  search          ,  stretch          ,  viz ) {
+define(     ['jquery',  'js/bccvl-stretch', 'js/bccvl-visualiser', 'bootstrap', 'jquery-tablesorter'],
+  function(   $      ,   stretch          ,  viz ) {
   // ==============================================================
     $(function() {
 
       viz.init();
-      search.init();
       stretch.init({ topPad: 60, bottomPad: 10 });
 
       $('.bccvl-datasetstable').tablesorter({
