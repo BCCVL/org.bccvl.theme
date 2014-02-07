e0286d198fb08161ac525aea5b440bc198b544cc
Daniel Yazbek <daniel@intersect.org.au>
2014-02-07 13:26:51 +1100

    Add 'Import Dataset' page

---
+++ org/bccvl/theme/theme/js/dataset-import.js
@@ -0,0 +1,16 @@
+
+//
+// main JS for the dataset iomport page.
+//
+define(     ['jquery', 'js/bccvl-search',  'bootstrap', ],
+  function(   $      ,  search  ) {
+  // ==============================================================
+    $(function() {
+
+      search.init();
+
+    });
+
+  // ==============================================================
+  }
+);
