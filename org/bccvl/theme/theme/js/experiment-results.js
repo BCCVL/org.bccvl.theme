c02e10dd83fd97e06bf6109434d54d7837ae0c2c
Khanh Nguyen <khanh@intersect.org.au>
2014-02-07 15:22:33 +1100

    Add dynamic progress notification when running an experiment on results page

--- org/bccvl/theme/theme/js/experiment-results.js
+++ org/bccvl/theme/theme/js/experiment-results.js
@@ -7,10 +7,78 @@ define(     ['jquery', 'js/bccvl-stretch', 'js/bccvl-visualiser', 'bootstrap'],
     // ==============================================================
         $(function() {
 
-            stretch.init({ topPad: 60, bottomPad: 10 });
-            viz.init();
-
+          stretch.init({ topPad: 60, bottomPad: 10 });
+          viz.init();
+          
+          // Check to see if the experiment is already completed or not before start polling
+          var experimentStatus = $(".bccvl-expstatus").attr('status');
+          if ( experimentStatus != 'Completed' && experimentStatus != 'Failed'){
+            pollExperimentStatus();  
+          }
         });
+
+        // Poll /jm/getJobStatus for the status of the experiments
+        // This endpoint returns the status of each algorithm
+        function pollExperimentStatus() {
+          var url = document.URL.replace('/view', '') + '/jm/getJobStatus'
+
+          // ajax call
+          $.get(url).done(function(data){
+
+            var completed = true;
+            var running = false;
+            var html = '';
+
+            data.forEach(function(job){
+              var algorithm = job[0];
+              var status = job[1];
+              var icon;
+
+              // Creates the html for the Algorithm and icon representing the status
+              if (status != 'Completed' && status != 'Failed'){
+                completed = false;
+                icon = '<i class="bccvl-small-spinner" title="' + status + '"></i>'
+              } 
+              else if (status == 'Completed') {
+                icon = '<i class="icon-ok" title="Completed"></i>'
+              }
+              else {
+                icon = '<i class="icon-exclamation-sign" title="Failed"></i>'
+              }
+
+              // Determine if there are any running algorithms
+              if (status == 'Running'){
+                running = true;
+              }
+
+              html += '<span>' + algorithm + ': ' + icon + '</span>';
+            })
+
+            html = '<div class="algorithm-status">' + html + '</div>'
+
+            // update the status in html
+            if (html != ''){
+              $(".algorithm-status").remove();
+              $(".bccvl-expstatus").append(html);  
+            }
+
+            // Continue to poll until all algorithms are done
+            if (!completed){
+              if (running){
+                $(".alert-queued-text").empty()
+                $(".alert-queued-text").text('running')
+              }
+              setTimeout(function() { pollExperimentStatus(); }, 5000);
+            }
+            else {
+              // refresh the page when the experiment is completed
+              location.reload(); 
+            }
+
+          })
+          
+        }
+
     // ==============================================================
     }
 );
