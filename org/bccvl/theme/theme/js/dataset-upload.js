d7a01d7544fdf5a4b5cc7408c1bb302807f0f343
Daniel Yazbek <daniel@intersect.org.au>
2014-02-11 16:39:58 +1100

    Integrate the datasets upload page

--- org/bccvl/theme/theme/js/dataset-upload.js
+++ org/bccvl/theme/theme/js/dataset-upload.js
@@ -7,8 +7,20 @@ define(     ['jquery', 'bootstrap', 'bootstrap-fileupload'],
     // ==============================================================
         $(function() {
 
+        	console.log('page behavior loaded');
+
             // gets the file upload handling working
             $('.fileupload').fileupload();
+
+        	$('#upload-species').click(function(e) {
+            	$('div.bccvl-datasetuploadlayerform').addClass('hidden');
+            	$('div.bccvl-datasetuploadspeciesform').removeClass('hidden');
+            });
+
+			$('#upload-layer').click(function(e) {
+            	$('div.bccvl-datasetuploadlayerform').removeClass('hidden');
+            	$('div.bccvl-datasetuploadspeciesform').addClass('hidden');
+            });        	
 
         });
     // ==============================================================
