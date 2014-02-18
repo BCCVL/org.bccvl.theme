2938d1a5d41041b2d43d3e5c0111676898fc757a
Khanh Nguyen <khanh@intersect.org.au>
2014-02-18 15:56:31 +1100

    Fix parsley validations on upload dataset forms (it was trying to validate both forms at once)

--- org/bccvl/theme/static/js/dataset-upload.js
+++ org/bccvl/theme/static/js/dataset-upload.js
@@ -2,7 +2,7 @@
 //
 // main JS for the dataset upload page.
 //
-define(     ['jquery', 'js/bccvl-form-validator', 'bootstrap', 'bootstrap-fileupload'],
+define(     ['jquery', 'js/bccvl-form-validator', 'bootstrap', 'bootstrap-fileupload', 'parsley'],
     function( $      ,  formvalidator ) {
     // ==============================================================
         $(function() {
@@ -23,6 +23,27 @@ define(     ['jquery', 'js/bccvl-form-validator', 'bootstrap', 'bootstrap-fileup
             	$('div.bccvl-datasetuploadspeciesform').addClass('hidden');
                 $('a#upload-dataset-title').text('Upload Environmental Layer');
             });        	
+
+            $('form#addspecies').parsley({
+                excluded:     'input[type=hidden], :disabled',  // effectively enable validation of input type file (this is disbaled by default)
+                focus:        'none',                           // don't switch focus to errors (we do that manually below)
+                successClass: 'success',                        // use these two Bootstrap classes for the error
+                errorClass:   'error',                          // and no-error states, and it'll look pretty.
+                errors: {
+                    // this error handling and elements make parsley errors Bookstrap friendly
+                    classHandler: function(el) { return $(el).closest('.control-group'); },
+                    container: function(el) {
+                        var $controlGroup = $(el).closest('.control-group');
+                        var $tableHeader = $controlGroup.find('th');
+                        // if the element is in a table, use the table header..
+                        if ($tableHeader.length > 0) return $tableHeader;
+                        // otherwise use the controlGroup
+                        return $controlGroup;
+                    },
+                    errorsWrapper: '<span class=\"help-inline bccvl-formerror\"></span>',
+                    errorElem:     '<span></span>'
+                }
+            });
 
         });
     // ==============================================================
