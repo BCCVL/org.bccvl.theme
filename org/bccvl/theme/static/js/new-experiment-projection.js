d2edb183c9a1c2f3ab0ddb28ec898052d28885c9
Khanh Nguyen <khanh@intersect.org.au>
2014-02-25 13:53:23 +1100

    Add ajax to new projection experiment to detect the number of available projections

--- org/bccvl/theme/static/js/new-experiment-projection.js
+++ org/bccvl/theme/static/js/new-experiment-projection.js
@@ -4,7 +4,8 @@
 define(     ['jquery', 'js/bccvl-visualiser', 'js/bccvl-wizard-tabs', 'js/bccvl-stretch', 'js/bccvl-fadeaway', 'js/bccvl-dimension-equation', 'js/bccvl-form-validator', 'jquery-tablesorter'],
     function( $      ,  viz                 ,  wiztabs              ,  stretch          , fadeaway           ,  dimensions                  ,  formvalidator) {
     // ==============================================================
-
+        // global list for projection validation
+        var cachedProjectionDatasets = [];
         // do the work
         $(function() {
 
@@ -17,7 +18,7 @@ define(     ['jquery', 'js/bccvl-visualiser', 'js/bccvl-wizard-tabs', 'js/bccvl-
             fadeaway.init();
 
             // init the dimension chooser thingy
-            dimensions.init();
+            // dimensions.init();
 
             // hook up the wizard buttons
             wiztabs.init();
@@ -31,7 +32,98 @@ define(     ['jquery', 'js/bccvl-visualiser', 'js/bccvl-wizard-tabs', 'js/bccvl-
                 sortList: [[1,1]]
             });
 
-        });
+            // validate onload since some fields may already be filled in
+            validateProjection();
+
+            // bind listeners on the checkboxes in the Projection tab
+            $("[id^=form-widgets-years-]").change(function () {
+                validateProjection();
+            });
+
+            $("[id^=form-widgets-emission_scenarios-]").change(function () {
+                validateProjection();
+            });
+
+            $("[id^=form-widgets-climate_models-]").change(function () {
+                validateProjection();
+            });
+        }); 
+
+        
+        // This calls /dm/getProjectionDatasets - which currently returns the number of available
+        // datasets in the system with the selected filters.
+        // Added a cache like logic to speed things up.
+        function validateProjection() {
+            var checkedYears = $("[id^=form-widgets-years-]:checked");
+            var checkedEmissionScenarios = $("[id^=form-widgets-emission_scenarios-]:checked");
+            var checkedClimateModels = $("[id^=form-widgets-climate_models-]:checked");
+
+            var yearKey, emissionScenarioKey, climateModelKey;
+
+            var numDatasets = 0;
+            var combinations = [];
+
+            // get a list of combinations
+            checkedYears.each(function (yearIndex) {
+                var year = $(this);
+
+                checkedEmissionScenarios.each(function (emissionIndex) {
+                    var emissionScenario = $(this);
+
+                    checkedClimateModels.each(function (climateModelIndex) {
+                        var climateModel = $(this);
+                        yearKey = year.attr('name');
+                        emissionScenarioKey = emissionScenario.attr('name');
+                        climateModelKey = climateModel.attr('name');
+
+                        var datasetParams = {};
+                        datasetParams[yearKey] = year.attr('value');
+                        datasetParams[emissionScenarioKey] = emissionScenario.attr('value');
+                        datasetParams[climateModelKey] = climateModel.attr('value');
+                        console.log(climateModel.attr('value'))
+                        combinations.push(datasetParams);
+                    })
+
+                })
+
+            })
+
+            // make ajax call to the endpoint with each combination
+            // endpoint returns a int value of the number of datasets available
+            if (combinations.length > 0) {
+                combinations.forEach(function(datasetParams) {
+                    var isCached = false;
+
+                    // If we've already done the ajax call then don't do it again.
+                    cachedProjectionDatasets.forEach(function(cachedDataset) {
+                        if (datasetParams[yearKey] == cachedDataset[yearKey] &&
+                            datasetParams[emissionScenarioKey] == cachedDataset[emissionScenarioKey] &&
+                            datasetParams[climateModelKey] == cachedDataset[climateModelKey]) {
+                            numDatasets += cachedDataset['numberOfAvailableDatasets'];
+                            isCached = true;
+                        }
+                    })
+
+                    if (!isCached) {
+                        $.ajax({
+                            url: portal_url + '/dm/getProjectionDatasets',
+                            data: datasetParams,
+                            async: false,
+                            success: function (data) {
+                                numDatasets += data;
+
+                                // store data so we don't need to call this again for these params
+                                datasetParams['numberOfAvailableDatasets'] = data;
+                                cachedProjectionDatasets.push(datasetParams);
+                            }
+                        });
+                    }
+                });
+            }
+
+            // Update the number displayed on the page.
+            $('.bccvl-available-projections').text(numDatasets);
+        }
     // ==============================================================
     }
 );
