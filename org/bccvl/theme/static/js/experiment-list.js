85ea891d3ca5711f3bc65ee37e013177aecc57d3
Khanh Nguyen <khanh@intersect.org.au>
2014-03-13 15:19:03 +1100

    BCCVLII-72: As a user I would like to share my experiments

--- org/bccvl/theme/static/js/experiment-list.js
+++ org/bccvl/theme/static/js/experiment-list.js
@@ -2,10 +2,11 @@
 //
 // main JS for the experiment list page.
 //
-define(     ['jquery', 'bootstrap', 'jquery-tablesorter'],
-    function( $) {
+define(     ['jquery', 'js/bccvl-sharing-modal', 'bootstrap', 'jquery-tablesorter'],
+    function( $      ,  sharing) {
     // ==============================================================
         $(function() {
+            sharing.init();
             $('.bccvl-experimenttable').tablesorter({
                 headers: { 
                     4: { sorter: false } // should be link column
