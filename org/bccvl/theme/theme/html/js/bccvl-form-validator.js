d2affecbd851af9f6a1d536b2d63363f41bf4176
Daniel Yazbek <daniel@intersect.org.au>
2014-05-09 15:30:31 +1000

    BCCVLII-175 Disallow scientific notation in integer fields

--- org/bccvl/theme/theme/html/js/bccvl-form-validator.js
+++ org/bccvl/theme/theme/html/js/bccvl-form-validator.js
@@ -21,14 +21,7 @@ define(
                     // Regexp('^-?(?:\\d+|\\d{1,3}(?:,\\d{3})+)?(?:\\.\\d+)?$');
                     break;
                 case 'integer':
-                    assert = new Validator.Assert().Callback(function(value) {
-                      if (value.indexOf(".") >= 0) {
-                        return false;
-                      }
-                      var numb = Number(value);
-                      return Number.isInteger(numb);
-                    });
-                    //assert = new Validator.Assert().Regexp('^-?\\d+$');
+                    assert = new Validator.Assert().Regexp('^-?\\d+$');
                     break;
                 case 'digits':
                     assert = new Validator.Assert().Regexp('^\\d+$');
