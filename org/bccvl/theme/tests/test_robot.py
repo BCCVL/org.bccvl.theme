ba842bde6c4cb1c3bdb8af0391c45491601dda3a
Gerhard Weis <g.weis@griffith.edu.au>
2014-06-11 09:48:44 +1000

    fix tests

--- org/bccvl/theme/tests/test_robot.py
+++ org/bccvl/theme/tests/test_robot.py
@@ -2,7 +2,6 @@ import unittest2 as unittest
 
 import robotsuite
 from org.bccvl.theme.testing import BCCVL_THEME_FUNCTIONAL_TESTING
-from org.bccvl.theme.testing import BCCVL_THEME_ASYNC_FUNCTIONAL_TESTING
 from plone.testing import layered
 
 
@@ -10,6 +9,6 @@ def test_suite():
     suite = unittest.TestSuite()
     suite.addTests([
         layered(robotsuite.RobotTestSuite('test_first.robot'),
-                layer=BCCVL_THEME_ASYNC_FUNCTIONAL_TESTING),
+                layer=BCCVL_THEME_FUNCTIONAL_TESTING),
     ])
     return suite
