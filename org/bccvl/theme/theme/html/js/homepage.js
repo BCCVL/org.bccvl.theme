4df61d9b9c83ab0c965380cbf378f9d650c22024
Sam Wolski <s.wolski@griffith.edu.au>
2014-08-22 14:37:33 +1000

    Home page re-design.

---
+++ org/bccvl/theme/theme/html/js/homepage.js
@@ -0,0 +1,24 @@
+//
+// main JS for the login page.
+//
+define(     ['jquery', 'bootstrap'],
+    function( $) {
+    // ==============================================================
+        $(function() {
+            console.log('homepage scripts loaded.')
+
+            // delay iframe loading until after document is ready, resize on parent
+            $('iframe').each(function(){
+              $(this).attr('src',$(this).data('src'));
+              $(this).css('width', $(this).parent().innerWidth());
+              $(this).css('height', ( ($(this).parent().innerWidth()/16) * 9 ));
+            });
+
+            $( window ).resize(function() {
+                $(this).css('width', $(this).parent().innerWidth());
+              $(this).css('height', ( ($(this).parent().innerWidth()/16) * 9 ));
+            }); 
+        });
+    // ==============================================================
+    }
+);
