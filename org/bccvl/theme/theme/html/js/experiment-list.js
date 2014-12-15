b9e12f35282c29754d7c9905ae9db0a801417afa
Sam Wolski <s.wolski@griffith.edu.au>
2014-12-15 15:56:40 +1000

    Fixed experiment type labels, new experiment list layout and toggles.

--- org/bccvl/theme/theme/html/js/experiment-list.js
+++ org/bccvl/theme/theme/html/js/experiment-list.js
@@ -12,6 +12,24 @@ define(
                     4: { sorter: false } // should be link column
                 }
             });
+            $('.bccvl-experimenttable .experiment-more-info').hide(0, function(){
+                $(this).after('<p><a href="javascript:void(0)" class="more-info show-info">More Info<i class="fa fa-angle-down" style="margin-left:.2em;"></i></a></p>');
+            });
+            $('.bccvl-experimenttable').on('click', '.more-info.show-info', function(){
+                var button = $(this);
+                button.parents('td').find('.experiment-more-info').slideDown(300, function(){
+                    button.html('Hide<i class="fa fa-angle-up" style="margin-left:.2em;"></i>');
+                    button.addClass('hide-info').removeClass('show-info');
+                });
+            });
+            $('.bccvl-experimenttable').on('click', '.more-info.hide-info', function(){
+                var button = $(this);
+                button.parents('td').find('.experiment-more-info').slideUp(300, function(){
+                    button.html('More Info<i class="fa fa-angle-down" style="margin-left:.2em;"></i>');
+                    button.addClass('show-info').removeClass('hide-info');
+                });
+            });
+
             console.log('page behaviour loaded.');
         });
         // ==============================================================
