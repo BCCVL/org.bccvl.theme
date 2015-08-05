6dc631f9f136d51cc10f7272ca60816e3719f2f0
Sam Wolski <s.wolski@griffith.edu.au>
2015-08-05 14:40:46 +1000

    Help text and issue with color range assignment fixed.

--- org/bccvl/theme/theme/html/js/bccvl-visualiser-overlay.js
+++ org/bccvl/theme/theme/html/js/bccvl-visualiser-overlay.js
@@ -22,6 +22,7 @@ define(['jquery', 'js/bccvl-preview-layout', 'openlayers3', 'ol3-layerswitcher',
             event.preventDefault();
 
             var uuid = $(this).data('uuid');
+            var colorName = $('.olLegend label[data-uuid="'+uuid+'"]').data('color-name');
 
             visLayers.getLayers().forEach(function (lyr) {
                 if (lyr.get('uuid') == uuid){
@@ -29,7 +30,14 @@ define(['jquery', 'js/bccvl-preview-layout', 'openlayers3', 'ol3-layerswitcher',
                 }          
             });
 
-            $('.olLegend label[data-uuid="'+$(this).data('uuid')+'"]').remove();
+            $.each(usedStyleArray, function(i){
+                if($(this)[0].name == colorName){
+                    styleArray.unshift(usedStyleArray[i]);
+                    usedStyleArray.splice(i,1);
+                }
+            });
+
+            $('.olLegend label[data-uuid="'+uuid+'"]').remove();
             $(this).removeClass('bccvl-remove-viz').addClass('bccvl-compare-viz');
             $(this).find('i').removeClass('icon-eye-close').addClass('icon-eye-open');
         });
@@ -60,54 +68,66 @@ define(['jquery', 'js/bccvl-preview-layout', 'openlayers3', 'ol3-layerswitcher',
             { "minVal": 0, "maxVal": 1, "steps": 20,
               "startpoint": {r:255,g:255,b:255},
               "midpoint": {r:139,g:208,b:195},
-              "endpoint": {r:18,g:157,b:133}
+              "endpoint": {r:18,g:157,b:133},
+              "name":'aquaGreen'
             },
             { "minVal": 0,"maxVal" :1, "steps": 20,
               "startpoint": {r:255,g:255,b:255},
               "midpoint": {r:233,g:170,b:129},
-              "endpoint": {r:210,g:96,b:19}
+              "endpoint": {r:210,g:96,b:19},
+              "name":'redOrange'
             },
             { "minVal": 0, "maxVal": 1, "steps": 20,
               "startpoint": {r:255,g:255,b:255},
               "midpoint": {r:255,g:172,b:236},
-              "endpoint": {r:247,g:108,b:215}
+              "endpoint": {r:247,g:108,b:215},
+              "name":'magenta'
             },
             { "minVal": 0, "maxVal": 1, "steps": 20,
               "startpoint": {r:255,g:255,b:255},
               "midpoint": {r:248,g:225,b:135},
-              "endpoint": {r:241,g:196,b:15}
+              "endpoint": {r:241,g:196,b:15},
+              "name":'yellowOrange'
             },
             { "minVal": 0, "maxVal": 1, "steps": 20,
               "startpoint": {r:255,g:255,b:255},
               "midpoint": {r:198,g:162,b:214},
-              "endpoint": {r:143,g:76,b:176}
+              "endpoint": {r:143,g:76,b:176},
+              "name":'purple'
             },
             { "minVal": 0, "maxVal": 1, "steps": 20,
               "startpoint": {r:255,g:255,b:255},
               "midpoint": {r:154,g:164,b:175},
-              "endpoint": {r:48,g:71,b:94}
+              "endpoint": {r:48,g:71,b:94},
+              "name":'darkNavy'
             },  
             { "minVal": 0, "maxVal": 1, "steps": 20,
               "startpoint": {r:255,g:255,b:255},
               "midpoint": {r:151,g:229,b:184},
-              "endpoint": {r:45,g:195,b:108}
+              "endpoint": {r:45,g:195,b:108},
+              "name":'brightGreen'
             },
             { "minVal": 0, "maxVal": 1, "steps": 20,
               "startpoint": {r:255,g:255,b:255},
               "midpoint": {r:154,g:203,b:237},
-              "endpoint": {r:47,g:150,b:220}
+              "endpoint": {r:47,g:150,b:220},
+              "name":'brightBlue'
             },
             { "minVal": 0, "maxVal": 1, "steps": 20,
               "startpoint": {r:255,g:255,b:255},
               "midpoint": {r:223,g:156,b:149},
-              "endpoint": {r:192,g:57,b:43}
+              "endpoint": {r:192,g:57,b:43},
+              "name":'deepRed'
             },
             { "minVal": 0, "maxVal": 1, "steps": 20,
               "startpoint": {r:255,g:255,b:255},
               "midpoint": {r:154,g:154,b:154},
-              "endpoint":{r:47,g:47,b:47}
+              "endpoint":{r:47,g:47,b:47},
+              "name":'black'
             }
         ];
+
+        var usedStyleArray = [];
 
 
         renderBase($('.bccvl-preview-pane:visible').attr('id'));
@@ -123,13 +143,13 @@ define(['jquery', 'js/bccvl-preview-layout', 'openlayers3', 'ol3-layerswitcher',
             $('#'+id+' .ol-viewport').append(legend);
         }
 
-        function addLayerLegend(layername, color, uuid){  
+        function addLayerLegend(layername, color, uuid, colorName){  
             if (color == 'occurrence'){
                 $('.olLegend').append('<label data-uuid="'+uuid+'" style="padding-top:1px;"><i style="color:red;text-align:center;margin-top:3px;" class="fa fa-circle"></i>&nbsp;'+layername+'</label>');
                 $('.olLegend').show(0);
             } else {
                 var colorRGB = 'rgba('+color.r+','+color.g+','+color.b+',1)';
-                $('.olLegend').append('<label data-uuid="'+uuid+'"><i style="background:'+colorRGB+'"></i>&nbsp;'+layername+'</label>');
+                $('.olLegend').append('<label data-uuid="'+uuid+'" data-color-name="'+colorName+'"><i style="background:'+colorRGB+'"></i>&nbsp;'+layername+'</label>');
                 $('.olLegend').show(0);
             }
         }
@@ -328,11 +348,14 @@ define(['jquery', 'js/bccvl-preview-layout', 'openlayers3', 'ol3-layerswitcher',
                             layerName = layerName || layer_vocab[layer.layer] || layer.layer || layer.filename;
                             // TODO: double check ... we should only have probability rasters here
                             var max = vizcommon.roundUpToNearestMagnitude(layer.max);
-                            var styleObj = $.extend({}, styleArray[numLayers]);
+                            var styleObj = $.extend({}, styleArray[0]);
                             styleObj.maxVal = max;
                             newLayer = vizcommon.createLayer(uuid, data, layer, layerName, 'wms', true, styleObj);
 
-                            addLayerLegend(layerName, styleArray[numLayers].endpoint, uuid);
+                            addLayerLegend(layerName, styleArray[0].endpoint, uuid, styleArray[0].name);
+
+                            usedStyleArray.push(styleArray[0]);
+                            styleArray.splice(0, 1);
                             
                             // add layer to layer group
                             visLayers.getLayers().push(newLayer);
